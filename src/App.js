dda25702936b1dfe60540effc7995b351f078662
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-04-18 13:24:41 +0200

    Eliminar archivos de prueba y agregar dependencias de Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
+import TasksForm from './components/TasksForm';
+import TasksList from './components/TasksList';
 
 function App() {
+  const tasksState = useSelector(state => state.tasks);
+
+  console.log(tasksState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo!</h1>
+
+      <TasksForm />
+      <TasksList />
     </div>
   );
 }
